ade8b5c02ee48dcb8e7676ecc8069bee9ac786b4
Eric Huber <ericmhuber20@gmail.com>
2022-12-04 11:23:49 -0600

    refactored calculator.rb with rubocop edits

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -24,26 +24,21 @@ name = ''
 loop do
   name = gets.chomp
 
-  if name.empty?
-    prompt 'Make sure to use a valid name.'
-  else
-    break
-  end
+  break unless name.empty?
+
+  prompt 'Make sure to use a valid name.'
 end
 
 prompt "Hi, #{name}!"
-
+number1 = ''
 loop do
-  number1 = ''
   loop do
     prompt 'What is the first number?'
     number1 = gets.chomp
 
-    if valid_number(number1)
-      break
-    else
-      prompt 'Invalid number'
-    end
+    break if valid_number(number1)
+
+    prompt 'Invalid number'
   end
 
   number2 = ''
@@ -51,11 +46,9 @@ loop do
     prompt 'What is the second number?'
     number2 = gets.chomp
 
-    if valid_number(number2)
-      break
-    else
-      prompt 'Invalid number'
-    end
+    break if valid_number(number2)
+
+    prompt 'Invalid number'
   end
 
   operator_prompt = <<-MSG
@@ -71,16 +64,14 @@ loop do
   operator = ''
   loop do
     operator = gets.chomp
-    if ['1', '2', '3', '4'].include?(operator)
-      break
-    else
-      prompt 'Must choose 1, 2, 3, or 4'
-    end
+    break if ['1', '2', '3', '4'].include?(operator)
+
+    prompt 'Must choose 1, 2, 3, or 4'
   end
 
   prompt "#{operation_to_message(operator)} the two numbers..."
 
-  result = case operator
+  result =  case operator
             when '1'
               number1.to_i + number2.to_i
             when '2'
@@ -88,13 +79,13 @@ loop do
             when '3'
               number1.to_i * number2.to_i
             when '4'
-              number1.to_f / number2.to_f
-          end
+              number1.to_f / number2.to_i
+            end
 
   prompt "The result is #{result}"
   prompt 'Another calculation?'
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
-        end
+end
 
 prompt 'Thank you'
