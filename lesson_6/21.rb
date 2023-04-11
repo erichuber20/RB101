1f1cda9072950b42aecba5d4ebda689850a0e208
erichuber20 <112355932+erichuber20@users.noreply.github.com>
2023-04-10 23:36:12 -0500

    21 initial work

--- lesson_6/21.rb
+++ lesson_6/21.rb
@@ -10,23 +10,22 @@ def initialize_deck
 end
 
 def total(cards)
-  # cards = [['H', '3'], ['S', 'Q'], ... ]
   values = cards.map { |card| card[1] }
 
   sum = 0
   values.each do |value|
-    if value == "A"
+    if value == 'A'
       sum += 11
-    elsif value.to_i == 0 # J, Q, K
+    elsif value.to_i == 0
       sum += 10
     else
       sum += value.to_i
     end
   end
 
-  # correct for Aces
-  values.select { |value| value == "A" }.count.times do
-    sum -= 10 if sum > 21
+  # correct Aces
+  values.select { |value| value == 'A' }.count.times do
+    sum -=10 if sum > 21
   end
 
   sum
@@ -36,7 +35,6 @@ def busted?(cards)
   total(cards) > 21
 end
 
-# :tie, :dealer, :player, :dealer_busted, :player_busted
 def detect_result(dealer_cards, player_cards)
   player_total = total(player_cards)
   dealer_total = total(dealer_cards)
@@ -72,47 +70,45 @@ def display_result(dealer_cards, player_cards)
 end
 
 def play_again?
-  puts "-------------"
   prompt "Do you want to play again? (y or n)"
-  answer = gets.chomp
+  answer= gets.chomp
   answer.downcase.start_with?('y')
 end
 
 loop do
-  prompt "Welcome to Twenty-One!"
+  prompt "Welcome to 21"
 
-  # initialize vars
+  #initialize variables
   deck = initialize_deck
   player_cards = []
   dealer_cards = []
 
-  # initial deal
+  #initial deal
   2.times do
     player_cards << deck.pop
     dealer_cards << deck.pop
   end
 
   prompt "Dealer has #{dealer_cards[0]} and ?"
-  prompt "You have: #{player_cards[0]} and #{player_cards[1]}, for a total of #{total(player_cards)}."
+  prompt "You have: #{player_cards[0]} and #{player_cards[1]}, totaling #{total(player_cards)}."
 
-  # player turn
+  #player turn
   loop do
     player_turn = nil
     loop do
-      prompt "Would you like to (h)it or (s)tay?"
+      prompt "Would you like to hit or stay?"
       player_turn = gets.chomp.downcase
-      break if ['h', 's'].include?(player_turn)
-      prompt "Sorry, must enter 'h' or 's'."
+      break if player_turn.start_with?('h') || player_turn.start_with?('s')
+      prompt "Sorry, must enter hit or stay."
     end
 
     if player_turn == 'h'
       player_cards << deck.pop
-      prompt "You chose to hit!"
-      prompt "Your cards are now: #{player_cards}"
-      prompt "Your total is now: #{total(player_cards)}"
+      prompt 'You hit!'
+      prompt "Your cards are now: #{player_cards}, totaling #{total(player_cards)}"
     end
 
-    break if player_turn == 's' || busted?(player_cards)
+    break if player_turn.start_with?('s') || busted?(player_cards)
   end
 
   if busted?(player_cards)
@@ -122,34 +118,32 @@ loop do
     prompt "You stayed at #{total(player_cards)}"
   end
 
-  # dealer turn
-  prompt "Dealer turn..."
+  #dealer turn
+  prompt "Dealer's turn..."
 
   loop do
     break if total(dealer_cards) >= 17
 
     prompt "Dealer hits!"
     dealer_cards << deck.pop
-    prompt "Dealer's cards are now: #{dealer_cards}"
+    prompt "Dealer's cards are now #{dealer_cards}"
   end
 
   if busted?(dealer_cards)
-    prompt "Dealer total is now: #{total(dealer_cards)}"
+    prompt "Dealer's total is now: #{total(dealers_cards)}"
     display_result(dealer_cards, player_cards)
     play_again? ? next : break
   else
     prompt "Dealer stays at #{total(dealer_cards)}"
   end
 
-  # both player and dealer stays - compare cards!
-  puts "=============="
-  prompt "Dealer has #{dealer_cards}, for a total of: #{total(dealer_cards)}"
-  prompt "Player has #{player_cards}, for a total of: #{total(player_cards)}"
-  puts "=============="
+  #both player and dealer stays - compare
+  prompt "Dealer has #{dealer_cards}, totaling #{total(dealer_cards)}"
+  prompt "You have #{player_cards}, totaling #{total(player_cards)}"
 
   display_result(dealer_cards, player_cards)
 
   break unless play_again?
 end
 
-prompt "Thank you for playing Twenty-One! Good bye!"
+prompt "Thanks for playing 21, good bye!"
