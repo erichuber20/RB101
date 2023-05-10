d6d8159f250caf9ef177c2778fa9b3ee48b1efd8
erichuber20 <112355932+erichuber20@users.noreply.github.com>
2023-05-09 22:50:02 -0500

    small probs work

--- test.rb
+++ test.rb
@@ -1,15 +1,44 @@
-def sum_square_difference(number)
-  numbers = (1..number).to_a 
+=begin
 
-  square_of_sum = (numbers.sum) ** 2
-  sum_of_squares = (numbers.map { |num| num ** 2 }).sum
-  
-  square_of_sum - sum_of_squares
+Write a function with the signature shown below:
+
+def is_int_array(arr)
+  true
 end
 
+- returns true if every element in an array is an integer or a float with no decimals.
+- returns true if array is empty.
+- returns false for every other input.
 
-p sum_square_difference(3) == 22
-   # -> (1 + 2 + 3)**2 - (1**2 + 2**2 + 3**2)
-p sum_square_difference(10) == 2640
-p sum_square_difference(1) == 0
-p sum_square_difference(100) == 25164150
+=end
+
+# test cases
+
+def is_int_array(arr)
+  return false unless arr.is_a? Array
+  arr.empty? || arr.all? { |num| num.is_a?(Numeric) && num.floor == num }
+end
+
+def is_int_array(arr)
+  arr.is_a?(Array) && arr.all? do
+    |x| x.is_a?(Integer) || x.is_a?(Float) && x.to_i == x
+  end
+end
+
+def is_int_array(arr)
+  return false if arr.class != Array
+  return true if arr.empty?
+  return arr.all? { |n| n.class == Integer || n == n.to_i }
+  false
+end
+
+p is_int_array([]) == true
+p is_int_array([1, 2, 3, 4]) == true
+p is_int_array([-11, -12, -13, -14]) == true
+p is_int_array([1.0, 2.0, 3.0]) == true
+p is_int_array([1, 2, nil]) == false
+p is_int_array(nil) == false
+p is_int_array("") == false
+p is_int_array([nil]) == false
+p is_int_array([1.0, 2.0, 3.0001]) == false
+p is_int_array(["-1"]) == false
